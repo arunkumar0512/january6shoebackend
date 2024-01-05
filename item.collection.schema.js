28bf85623ee16f116cc519d81752ccf46d0e2fd3
arunkumar0512 <140463247+arunkumar0512@users.noreply.github.com>
2024-01-06 00:29:56 +0530

    Update item.collection.schema.js

--- item.collection.schema.js
+++ item.collection.schema.js
@@ -1,18 +1,4 @@
-// import mongoose from "mongoose";
-
-// const user= new mongoose.Schema({
-
-//         name: String,
-//         size: String,
-//         price: String,
-//         quantity: String
-        
-
-// })
-
-// const itemcollection = mongoose.model(' itemcollection ', user)
-
-// export default  itemcollection ;     
+   
 
 import mongoose from "mongoose";
 
